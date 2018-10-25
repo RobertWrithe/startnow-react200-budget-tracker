df898e9a4169c2a37e6c5d663dc96136541dd9fa
Robert Barnett <therobertbarnett@gmail.com>
2018-10-25 11:47:48 -0700

    First Redux Store commit

---
+++ src/js/rootStore.js
@@ -0,0 +1,12 @@
+// import { creatStore } from 'redux';
+
+// function tempReducer () {
+//     return null;
+// }
+
+// const rootStore = createStore(
+//     tempReducer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+// export default rootStore;
